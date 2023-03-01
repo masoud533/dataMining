cec4efeb463568bc6da92ee933801deb1df02754
masoud533 <mr.masoud_mapar@outlook.com>
2023-03-02 00:05:37 +0330

    modified qura_1.py file -> V0.02

--- quera_1/qura_1.py
+++ quera_1/qura_1.py
@@ -14,14 +14,22 @@ data2 = data2.groupby('playerId').agg({
     'outcome': 'count',
 })
 print(data2[data2['outcome'] == data2.max()['outcome']].index.values[0])
+
 data3 = data.groupby('playerId').agg({
     'playType': 'count',
 })
 data3['outcome'] = data2['outcome']
 data3['persent'] = (data2['outcome'] / data3['playType']) * 100
 print(f'{data3[data3["persent"] == data3["persent"].max()].index.values[0]},{data3[data3["persent"] == data3["persent"].min()].index.values[0]}')
+
+data4 = data
+for i in range(0, len(data['playerId'])):
+  if data.iloc[i]['outcome'] in ['مهار توسط دروازه‌بان', 'گُل', 'گُل به‌خودی']:
+    data4 = data4.drop(index=i)
+print(np.shape(data4))
+print(np.shape(data))
 des0 = []
-for i in range(0 ,len(data)):
-  des0.append(round((((0 - data.iloc[i]['x']) ** 2) + ((0 - data.iloc[i]['y']) **2)) ** 0.5))
+for i in range(0 ,len(data4)):
+  des0.append(round((((0 - data4.iloc[i]['x']) ** 2) + ((0 - data4.iloc[i]['y']) ** 2)) ** 0.5))
 print(max(des0))
 
